7e41148e7c6f42001fb098ef23177d2948b350e8
Sweeper777 <sumulang@gmail.com>
2018-12-01 11:22:48 +0000

    Initial Commit

---
+++ BlackHoleGamePrototype/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BlackHoleGamePrototype
+//
+//  Created by Mulang Su on 01/12/2018.
+//  Copyright © 2018 Mulang Su. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
