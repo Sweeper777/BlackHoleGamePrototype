61de0c6a7834ff6e1caf7d762b557086e059eed4
Sweeper777 <sumulang@gmail.com>
2018-12-05 06:55:02 +0000

    added set subscript

--- BlackHoleGamePrototype/TriangularArray.swift
+++ BlackHoleGamePrototype/TriangularArray.swift
@@ -23,6 +23,16 @@ struct TriangularArray<T> : Sequence, ExpressibleByArrayLiteral {
             }
             return innerArray[row][index]
         }
+        
+        set {
+            if row < 0 || row >= innerArray.count {
+                return
+            }
+            if index < 0 || index > row {
+                return
+            }
+            innerArray[row][index] = newValue!
+        }
     }
     init(arrayLiteral elements: ArrayLiteralElement...) {
         rowCount = elements.count
