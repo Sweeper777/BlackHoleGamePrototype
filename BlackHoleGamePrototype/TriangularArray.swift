eee82bddbeb4fabcc55d23711e8551311d0288ac
Sweeper777 <sumulang@gmail.com>
2018-12-05 06:55:47 +0000

    added adjacentIndices

--- BlackHoleGamePrototype/TriangularArray.swift
+++ BlackHoleGamePrototype/TriangularArray.swift
@@ -38,6 +38,20 @@ struct TriangularArray<T> : Sequence, ExpressibleByArrayLiteral {
     func makeIterator() -> TriangularArray<T>._Iterator {
         return _Iterator(triangularArray: self, currentRow: -1, currentIndex: -1)
     }
+    
+    func adjacentIndices(forRow row: Int, index: Int) -> [(Int, Int)] {
+        let possibleIndices = [
+            (row - 1, index - 1),
+            (row - 1, index),
+            (row, index - 1),
+            (row, index + 1),
+            (row + 1, index),
+            (row + 1, index + 1),
+        ]
+        return possibleIndices.filter {
+            (0..<rowCount).contains($0.0) && (0...$0.0).contains($0.1)
+        }
+    }
     init(arrayLiteral elements: ArrayLiteralElement...) {
         rowCount = elements.count
         innerArray = elements
