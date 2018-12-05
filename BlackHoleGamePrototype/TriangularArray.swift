037bd513a50423cade608c0dc14afdd295baf69a
Sweeper777 <sumulang@gmail.com>
2018-12-05 06:54:32 +0000

    added initialisers

--- BlackHoleGamePrototype/TriangularArray.swift
+++ BlackHoleGamePrototype/TriangularArray.swift
@@ -5,5 +5,17 @@ struct TriangularArray<T> : Sequence, ExpressibleByArrayLiteral {
     var innerArray: [[T]]
     let rowCount: Int
     
+    init(rowCount: Int, defaultValue: T) {
+        self.rowCount = rowCount
+        self.innerArray = []
+        for i in 0..<rowCount {
+            innerArray.append(Array(repeating: defaultValue, count: i + 1))
+        }
+    }
+    
+    init(arrayLiteral elements: ArrayLiteralElement...) {
+        rowCount = elements.count
+        innerArray = elements
+    }
 }
 
