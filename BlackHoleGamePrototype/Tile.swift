1a8f9b8e899aaf245578d9cf9965f73ae29d19b0
Sweeper777 <sumulang@gmail.com>
2018-12-06 07:14:02 +0000

    added TriangularArray.emptyCount extension

--- BlackHoleGamePrototype/Tile.swift
+++ BlackHoleGamePrototype/Tile.swift
@@ -4,6 +4,18 @@ enum Tile {
     case blue(number: Int)
 }
 
+extension TriangularArray where T == Tile {
+    var emptyCount: Int {
+        return self.filter {
+            if case .empty = $0 {
+                return true
+            }
+            return false
+        }.count
+    }
+    
+}
+
 enum PlayerSide {
     case red
     case blue
