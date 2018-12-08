c8a8d5de4394bf3277ec13ec64079886e760a039
Sweeper777 <sumulang@gmail.com>
2018-12-08 10:04:10 +0000

    simple two player game

--- BlackHoleGamePrototype/main.swift
+++ BlackHoleGamePrototype/main.swift
@@ -1,12 +1,21 @@
-//
-//  main.swift
-//  BlackHoleGamePrototype
-//
-//  Created by Mulang Su on 01/12/2018.
-//  Copyright © 2018 Mulang Su. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+//let game = Game()
+//game.makeMove(row: 1, index: 0)
+//game.makeMove(row: 3, index: 2)
+//game.makeMove(row: 2, index: 1)
+//game.makeMove(row: 5, index: 3)
+//print(game)
 
+let game = Game()
+while (game.checkWin() == .undecided) {
+    print(game.currentTurn)
+    print(game)
+    var row = -1
+    var index = -1
+    repeat {
+        let coordinates = readLine()!.split(separator: " ")
+        row = Int(coordinates[0])!
+        index = Int(coordinates[1])!
+    } while (!game.makeMove(row: row, index: index))
+}
