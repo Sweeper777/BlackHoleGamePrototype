23aec2c28565db87ecae0403590f4eb959745ca1
Sweeper777 <sumulang@gmail.com>
2018-12-08 10:00:03 +0000

    added getAvailableMoves

--- BlackHoleGamePrototype/GameAI.swift
+++ BlackHoleGamePrototype/GameAI.swift
@@ -20,4 +20,17 @@ class GameAI {
             return -10000
         }
     }
+    
+    func getAvailableMoves() -> [(row: Int, index: Int)] {
+        var moves = [(row: Int, index: Int)]()
+        for i in 0..<game.boardSize {
+            for j in 0..<(i + 1) {
+                if game.canMakeMove(row: i, index: j) {
+                    moves.append((i, j))
+                }
+            }
+        }
+        return moves
+    }
+    
 }
