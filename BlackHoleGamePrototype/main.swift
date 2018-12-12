a070ac8fb4adc704c7b2286fc094798b87d99d81
Sweeper777 <sumulang@gmail.com>
2018-12-12 06:43:42 +0000

    extracted testGameMakeMove

--- BlackHoleGamePrototype/main.swift
+++ BlackHoleGamePrototype/main.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+func testGameMakeMove() {
+    let game = Game()
+    game.makeMove(row: 1, index: 0)
+    game.makeMove(row: 3, index: 2)
+    game.makeMove(row: 2, index: 1)
+    game.makeMove(row: 5, index: 3)
+    print(game)
+}
+
 
     print(game)
 }
