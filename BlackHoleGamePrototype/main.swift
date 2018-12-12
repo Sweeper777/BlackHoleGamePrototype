7bcb048aaa162324df490111ab7590ab03da4353
Sweeper777 <sumulang@gmail.com>
2018-12-12 06:43:25 +0000

    deleted stuff in main.swift

--- BlackHoleGamePrototype/main.swift
+++ BlackHoleGamePrototype/main.swift
@@ -1,21 +1,5 @@
 import Foundation
 
-//let game = Game()
-//game.makeMove(row: 1, index: 0)
-//game.makeMove(row: 3, index: 2)
-//game.makeMove(row: 2, index: 1)
-//game.makeMove(row: 5, index: 3)
-//print(game)
 
-let game = Game()
-while (game.checkWin() == .undecided) {
-    print(game.currentTurn)
     print(game)
-    var row = -1
-    var index = -1
-    repeat {
-        let coordinates = readLine()!.split(separator: " ")
-        row = Int(coordinates[0])!
-        index = Int(coordinates[1])!
-    } while (!game.makeMove(row: row, index: index))
 }
