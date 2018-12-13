8e260976000ab778594129ac75ab0eb4d732586f
Sweeper777 <sumulang@gmail.com>
2018-12-13 08:05:02 +0000

    tested game.canMakeMove

--- BlackHoleGamePrototypeTests/BlackHoleGamePrototypeTests.swift
+++ BlackHoleGamePrototypeTests/BlackHoleGamePrototypeTests.swift
@@ -11,4 +11,13 @@ class BlackHoleGamePrototypeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testGameCanMakeMove() {
+        let game = Game()
+        XCTAssertTrue(game.canMakeMove(row: 0, index: 0))
+        XCTAssertFalse(game.canMakeMove(row: 0 , index: 1))
+        XCTAssertTrue(game.canMakeMove(row: 1, index: 0))
+        XCTAssertTrue(game.makeMove(row: 1, index: 0))
+        XCTAssertFalse(game.canMakeMove(row: 1, index: 0))
+    }
+
 }
