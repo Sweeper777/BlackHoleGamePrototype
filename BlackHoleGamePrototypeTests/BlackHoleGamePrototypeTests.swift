d5b823134ca5c67feb0926aba3888a405871ee3a
Sweeper777 <sumulang@gmail.com>
2018-12-13 08:05:10 +0000

    tested game.makeMove

--- BlackHoleGamePrototypeTests/BlackHoleGamePrototypeTests.swift
+++ BlackHoleGamePrototypeTests/BlackHoleGamePrototypeTests.swift
@@ -20,4 +20,18 @@ class BlackHoleGamePrototypeTests: XCTestCase {
         XCTAssertFalse(game.canMakeMove(row: 1, index: 0))
     }
 
+    func testGameMakeMove() {
+        let game = Game()
+        XCTAssertEqual(game.board[0, 0]!, .empty)
+        XCTAssertEqual(game.currentNumber, 1)
+        XCTAssertEqual(game.currentTurn, .red)
+        game.makeMove(row: 0, index: 0)
+        XCTAssertEqual(game.board[0, 0]!, .red(number: 1))
+        XCTAssertEqual(game.currentNumber, 1)
+        XCTAssertEqual(game.currentTurn, .blue)
+        game.makeMove(row: 1, index: 0)
+        XCTAssertEqual(game.board[1, 0]!, .blue(number: 1))
+        XCTAssertEqual(game.currentNumber, 2)
+        XCTAssertEqual(game.currentTurn, .red)
+    }
 }
