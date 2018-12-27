ae5e01bd9f364430a75515d8ab408cd7c0640337
Sweeper777 <sumulang@gmail.com>
2018-12-27 09:34:16 +0800

    tested checkWinBlueWinsWithWalls

--- BlackHoleGamePrototypeTests/BlackHoleGamePrototypeTests.swift
+++ BlackHoleGamePrototypeTests/BlackHoleGamePrototypeTests.swift
@@ -134,4 +134,18 @@ class BlackHoleGamePrototypeTests: XCTestCase {
         game.board = board
         XCTAssertEqual(game.checkWin(), .draw)
     }
+    
+    func testCheckWinBlueWinsWithWalls() {
+        let board: TriangularArray<Tile> = [
+            [.red(number: 1)],
+            [.red(number: 2), .red(number: 3)],
+            [.red(number: 5), .red(number: 4), .red(number: 6)],
+            [.blue(number: 5), .blue(number: 6), .blue(number: 1), .red(number: 7)],
+            [.blue(number: 7), .wall, .wall, .blue(number: 3), .red(number: 8)],
+            [.blue(number: 9), .red(number: 10), .empty, .blue(number: 4), .red(number: 10), .red(number: 9)],
+            ]
+        let game = Game()
+        game.board = board
+        XCTAssertEqual(game.checkWin(), .blueWins)
+    }
 }
